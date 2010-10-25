f4d7cc57ef6ccb15a1d756f0738f2309fbebaaaa
andrew.rapp <andrew.rapp@b0d05278-eb4d-11dd-85a8-df16bb8a7048>
2010-10-25 02:57:18 +0000

    Removed dead code

--- src/com/rapplogic/xbee/xmpp/client/XmppXBeeConnection.java
+++ src/com/rapplogic/xbee/xmpp/client/XmppXBeeConnection.java
@@ -15,57 +15,13 @@ public class XmppXBeeConnection implements XBeeConnection {
 
 	private final static Logger log = Logger.getLogger(XBeeConnection.class);
 	
-	private int[] inputBuffer;
 	private int[] outputBuffer = new int[200];
-	private int readPosition;
-	private int inputPosition;
 	private int outPosition;
-	private int inBytesAvailable;
 	
 	private XBeeSink sink;
 	
 	private PipedOutputStream pos = new PipedOutputStream();
 	private PipedInputStream pis = new PipedInputStream();
-	
-//	private InputStream in = new InputStream() {
-//
-//		@Override
-//		public int read() throws IOException {
-//			
-//			synchronized (inputBuffer) {
-//				if (inBytesAvailable < 1) {
-//					// block
-//					try {
-//						log.debug("Input buffer is empty.. waiting for new packets");
-//						inputBuffer.wait();
-//					} catch (InterruptedException e) {
-//						log.warn("Input buffer wait interrupted");
-//					}
-//				}
-//				
-//				int ret = inputBuffer[readPosition];
-//				
-//				log.debug("read(): byte is " + ByteUtils.toBase16(ret) + ", pos is " + readPosition + ", bytes available is " + inBytesAvailable);
-//						
-//				// increment read position
-//				if (++readPosition == inputBuffer.length) {
-//					readPosition = 0;
-//				}
-//				
-//				inBytesAvailable--;
-//				
-//				return ret;				
-//			}
-//		}
-//	
-//		@Override
-//		public int available() {
-//			synchronized (inputBuffer) {
-//				return inBytesAvailable;	
-//			}
-//		}
-//		
-//	};
 	
 	private OutputStream out = new OutputStream() {
 
@@ -102,9 +58,8 @@ public class XmppXBeeConnection implements XBeeConnection {
 		}
 	};
 	
-	public XmppXBeeConnection(XBeeSink sink, int inBufferSize) {
+	public XmppXBeeConnection(XBeeSink sink) {
 		this.sink = sink;
-		inputBuffer = new int[inBufferSize];
 		
 		try {
 			pis.connect(pos);	
@@ -131,45 +86,6 @@ public class XmppXBeeConnection implements XBeeConnection {
 				this.notify();
 			}			
 		}
-		
-		// always write to buffer starting at inputPosition
-//		synchronized (inputBuffer) {
-//			boolean empty = inBytesAvailable == 0;
-//			
-//			inBytesAvailable+= packet.length;
-//			
-//			if (inputPosition + packet.length < (inputBuffer.length)) {
-//				System.arraycopy(packet, 0, inputBuffer, inputPosition, packet.length);
-//				
-//				if (inputPosition + packet.length == inputBuffer.length -1) {
-//					inputPosition = 0;
-//				} else {
-//					inputPosition+= packet.length;
-//				}
-//			} else if (inputPosition == inputBuffer.length - 1) {
-//				log.debug("at end of input buffer, flipping");
-//				System.arraycopy(packet, 0, inputBuffer, 0, packet.length);
-//				inputPosition = packet.length;
-//			} else {
-//				// split
-//				log.debug("splittng packet across input buffer");
-//				System.arraycopy(packet, 0, inputBuffer, inputPosition, inputBuffer.length - inputPosition - 1);
-//				System.arraycopy(packet, 0, inputBuffer, 0, packet.length - (inputBuffer.length - inputPosition - 1));
-//				inputPosition = packet.length - (inputBuffer.length - inputPosition - 1);
-//			}
-//			
-//			// move inputPosition
-//			
-//			if (empty) {
-//				log.debug("Notify threads blocking on read() that new data is available: " + inBytesAvailable + " bytes");
-//				inputBuffer.notify();
-//			}
-//			
-//			// critical: notify XBee that data is available
-//			synchronized(this) {
-//				this.notify();
-//			}
-//		}
 	}
 	
 	public void close() {
